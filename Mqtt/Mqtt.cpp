6035dcaac303c76269a284ff38e6ad3efd759fff
MarcoFlo <MarcoFlo@Marco-Florian>
2019-04-02 21:58:47 +0200

    Mqtt client working with mosquitto broker

--- Mqtt/Mqtt.cpp
+++ Mqtt/Mqtt.cpp
@@ -10,60 +10,69 @@
 #include <WiFi.h>
 #include <MQTT.h>
 
-const char ssid[] = "ssid";
-const char pass[] = "pass";
+const char ssid[] = "TISCALI-Moschettieri";
+const char pass[] = "Ciao33trentini!";
+const char hostname[] = "192.168.1.143";
 
 WiFiClient net;
 MQTTClient client;
 
 unsigned long lastMillis = 0;
+unsigned long elapsedTime = 0;
 
 void connect() {
-  Serial.print("checking wifi...");
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(1000);
-  }
+	Serial.print("checking wifi...");
+	while (WiFi.status() != WL_CONNECTED) {
+		Serial.print(".");
+		delay(1000);
+	}
 
-  Serial.print("\nconnecting...");
-  while (!client.connect("arduino", "try", "try")) {
-    Serial.print(".");
-    delay(1000);
-  }
+	Serial.print("\nconnecting...");
 
-  Serial.println("\nconnected!");
+	while (!client.connect("esp32", false)) {
+			Serial.print(".");
+		delay(1000);
+	}
 
-  client.subscribe("/hello");
-  // client.unsubscribe("/hello");
+	Serial.println("\nconnected!");
+
+	client.subscribe("/time");
+	// client.unsubscribe("/hello");
 }
 
 void messageReceived(String &topic, String &payload) {
-  Serial.println("incoming: " + topic + " - " + payload);
+	Serial.println("incoming: " + topic + " - " + payload);
 }
 
 void setup() {
-  Serial.begin(115200);
-  WiFi.begin(ssid, pass);
+	Serial.begin(115200);
+	delay(1000);
 
-  // Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
-  // You need to set the IP address directly.
-  client.begin("broker.shiftr.io", net);
-  client.onMessage(messageReceived);
+	WiFi.begin(ssid, pass);
 
-  connect();
+	// Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
+	// You need to set the IP address directly.
+
+	client.begin(hostname,1884, net);
+	client.onMessage(messageReceived);
+
+	connect();
 }
 
 void loop() {
-  client.loop();
-  delay(10);  // <- fixes some issues with WiFi stability
+	client.loop();
+	delay(10);  // <- fixes some issues with WiFi stability
 
-  if (!client.connected()) {
-    connect();
-  }
+	if (!client.connected()) {
+		connect();
+	}
 
-  // publish a message roughly every second.
-  if (millis() - lastMillis > 1000) {
-    lastMillis = millis();
-    client.publish("/hello", "world");
-  }
+	// publish a message roughly every second.
+	elapsedTime = (millis() - lastMillis);
+	if (elapsedTime > 1000) {
+
+		client.publish("/time", String(elapsedTime));
+		lastMillis = millis();
+
+	}
 }
