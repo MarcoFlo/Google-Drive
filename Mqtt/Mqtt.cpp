64478270c7515ff6cdea256f7035f1c5a72422f1
florianMarco <florian.marco@live.com>
2019-04-07 23:24:11 +0200

    Text reformat

--- Mqtt/Mqtt.cpp
+++ Mqtt/Mqtt.cpp
@@ -21,27 +21,31 @@ unsigned long lastMillis = 0;
 unsigned long elapsedTime = 0;
 
 void connect() {
-	Serial.print("checking wifi...");
+	Serial.print("connecting to wifi...");
 	while (WiFi.status() != WL_CONNECTED) {
 		Serial.print(".");
 		delay(1000);
 	}
 
-	Serial.print("\nconnecting...");
+	Serial.print("\nWifi connected\n");
+	Serial.print("\nconnecting to Mqtt broker...");
 
 	while (!client.connect("esp32", false)) {
 			Serial.print(".");
 		delay(1000);
 	}
 
-	Serial.println("\nconnected!");
+	Serial.println("\nconnected to the broker!");
 
 	client.subscribe("/time");
 	// client.unsubscribe("/hello");
 }
 
+
 void messageReceived(String &topic, String &payload) {
-	Serial.println("incoming: " + topic + " - " + payload);
+	Serial.println("incoming msg " + topic + " - " + payload);
+
+	//Serial.println("incoming msg: " + " topic : " + topic + "  payload : " + payload + " ");
 }
 
 void setup() {
@@ -53,7 +57,7 @@ void setup() {
 	// Note: Local domain names (e.g. "Computer.local" on OSX) are not supported by Arduino.
 	// You need to set the IP address directly.
 
-	client.begin(hostname,1884, net);
+	client.begin(hostname,1883, net);
 	client.onMessage(messageReceived);
 
 	connect();
