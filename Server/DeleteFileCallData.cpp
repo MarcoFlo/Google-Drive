6e2a25308b4f12418cf7faec4e9464b2964b7355
florianMarco <florian.marco@live.com>
2019-12-13 18:55:43 +0100

    [server] delete file su fileSystem

--- Server/DeleteFileCallData.cpp
+++ Server/DeleteFileCallData.cpp
@@ -33,17 +33,32 @@ void DeleteFileCallData::HandleFileCall(protobuf::FileClientMap &fileClientMap, 
             //se è tra i suoi file
             if ((*fileToBeDeleted).usernameo() == request_.usernameo()) {
                 //se ha l'autorizzazione
-                fileList->mutable_fileil()->erase(fileToBeDeleted);
-                UpdateFileClientMap(fileClientMap);
 
-                responder_.Finish(reply_, grpc::Status::OK, this);
+                if (remove(request_.identifier().c_str()) != 0) {
+                    std::cout << "It wasn't possible to delete the file" << std::endl; //probably not closed correctly
+                    responder_.Finish(reply_, grpc::Status::CANCELLED, this);
+                } else {
+                    //cancello da tutti le filesInfoList il file
+                    std::for_each(fileClientMap.mutable_fileclientmap()->begin(),
+                                  fileClientMap.mutable_fileclientmap()->end(), [&fileIdentifier](auto &pair) {
+                                auto fileToBeDeleted = std::find_if(pair.second.mutable_fileil()->begin(),
+                                                                    pair.second.mutable_fileil()->end(),
+                                                                    [&fileIdentifier](protobuf::FileInfo &file) {
+                                                                        return file.identifier() == fileIdentifier;
+                                                                    });
+                                pair.second.mutable_fileil()->erase(fileToBeDeleted);
+
+                            });
+                    UpdateFileClientMap(fileClientMap);
+
+                    responder_.Finish(reply_, grpc::Status::OK, this);
+                }
             }
         } else {
             //todo
             responder_.Finish(reply_, grpc::Status::OK, this);
 
         }
-
     } else {
         GPR_ASSERT(status_ == FINISH);
         delete this;
