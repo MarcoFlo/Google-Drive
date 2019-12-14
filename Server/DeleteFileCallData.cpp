08ef31e855421d66b2e1c5028be1db957cf7ebd4
florianMarco <florian.marco@live.com>
2019-12-14 18:19:38 +0100

    [server] DeleteFileCallData.cpp con casi di errore + riorganizzata

--- Server/DeleteFileCallData.cpp
+++ Server/DeleteFileCallData.cpp
@@ -16,6 +16,12 @@ DeleteFileCallData::DeleteFileCallData(protobuf::CharacterService::AsyncService 
 
 
 void DeleteFileCallData::HandleFileCall(protobuf::FileClientMap &fileClientMap, bool ok) {
+    //ok non viene controllato perchè la connessione viene Finish solo dal server
+    if (status_ == FINISH) {
+        delete this;
+        return;
+    }
+
     if (status_ == READ_CALLED) {
         std::cout << "Delete request" << std::endl;
         new DeleteFileCallData(service_, cq_);
@@ -33,11 +39,7 @@ void DeleteFileCallData::HandleFileCall(protobuf::FileClientMap &fileClientMap, 
             //se è tra i suoi file
             if ((*fileToBeDeleted).usernameo() == request_.usernameo()) {
                 //se ha l'autorizzazione
-
-                if (remove(request_.identifier().c_str()) != 0) {
-                    std::cout << "It wasn't possible to delete the file" << std::endl; //probably not closed correctly
-                    responder_.Finish(reply_, grpc::Status::CANCELLED, this);
-                } else {
+                if (remove(request_.identifier().c_str()) == 0) {
                     //cancello da tutti le filesInfoList il file
                     std::for_each(fileClientMap.mutable_fileclientmap()->begin(),
                                   fileClientMap.mutable_fileclientmap()->end(), [&fileIdentifier](auto &pair) {
@@ -52,16 +54,16 @@ void DeleteFileCallData::HandleFileCall(protobuf::FileClientMap &fileClientMap, 
                     UpdateFileClientMap(fileClientMap);
 
                     responder_.Finish(reply_, grpc::Status::OK, this);
+                } else {
+                    std::cout << "It wasn't possible to delete the file" << std::endl; //probably not closed correctly
+                    responder_.Finish(reply_, grpc::Status(grpc::StatusCode::INTERNAL, "Internal error"), this);
                 }
+            } else {
+                responder_.Finish(reply_, grpc::Status(grpc::StatusCode::PERMISSION_DENIED,
+                                                       "Un file può essere cancellato solo dal proprietario"), this);
             }
         } else {
-            //todo
-            responder_.Finish(reply_, grpc::Status::OK, this);
-
+            responder_.Finish(reply_, grpc::Status(grpc::StatusCode::NOT_FOUND, "File non presente"), this);
         }
-    } else {
-        GPR_ASSERT(status_ == FINISH);
-        delete this;
     }
 }
-
