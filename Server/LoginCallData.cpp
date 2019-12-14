4ba8025a0bcb62c83410ef237931fb01f7b6332c
florianMarco <florian.marco@live.com>
2019-12-14 19:33:59 +0100

    [server] Login riorganizzato

--- Server/LoginCallData.cpp
+++ Server/LoginCallData.cpp
@@ -17,29 +17,26 @@ void LoginCallData::Proceed(bool ok) {
                                this);
     } else if (status_ == PROCESS) {
         std::cout << "Received a Login request from: " << request_.username() << std::endl;
-
-        std::for_each(ctx_.auth_context()->begin(),
-                      ctx_.auth_context()->end(),
-                      [](const grpc::AuthProperty &elem) {
-                          std::cout << elem.first << "    " << elem.second << std::endl;
-                      });
-        std::for_each(ctx_.client_metadata().begin(), ctx_.client_metadata().end(),
-                      [](auto &elem) {
-                          std::cout << elem.first << "     " << elem.second << std::endl;
-                      });
-
-
+        status_ = FINISH;
         new LoginCallData(service_, cq_);
 
+//        std::for_each(ctx_.auth_context()->begin(),
+//                      ctx_.auth_context()->end(),
+//                      [](const grpc::AuthProperty &elem) {
+//                          std::cout << elem.first << "    " << elem.second << std::endl;
+//                      });
+//        std::for_each(ctx_.client_metadata().begin(), ctx_.client_metadata().end(),
+//                      [](auto &elem) {
+//                          std::cout << elem.first << "     " << elem.second << std::endl;
+//                      });
+
         const std::vector<grpc::string_ref> tokenV = ctx_.auth_context()->FindPropertyValues("token");
-        const grpc::string_ref token = tokenV.back();
+        const grpc::string_ref &token = tokenV.back();
 //      Se vogliamo restituire un Empty e lasciare il token nei metadati
 //      ctx_.AddInitialMetadata("identifier", {token.begin(), token.end()});
         std::string s = {token.begin(), token.end()};
         reply_.set_token(s);
-        status_ = FINISH;
         responder_.Finish(reply_, grpc::Status::OK, this);
-
     } else {
         GPR_ASSERT(status_ == FINISH);
 // Once in the FINISH state, deallocate ourselves (CallData).
