6fb2cc889801c3f41cfb4cf079c8abfc9fea08aa
florianMarco <florian.marco@live.com>
2020-05-02 10:58:09 +0200

    [server] Closes #29 -> si accedeva a un vettore vuoto

--- Client/main.cpp
+++ Client/main.cpp
@@ -6,39 +6,21 @@ int main(int argc, char **argv) {
     CharacterClient client;
 
     /*
-    protobuf::User user_share;
-    user_share.set_username("provashare@test.it");
-    user_share.set_password("1234");
-    user_share.set_passwordr("1234");
-    client.Register(user_share);
-
     protobuf::User user;
     user.set_username("provabase@test.it");
     user.set_password("1234");
     user.set_passwordr("1234");
     client.Register(user);
-
-//    SharedEditor editor(client, user);
-
-    client.Login(user);
-    client.Logout();
     client.Login(user);
 
     protobuf::FileName fileName;
     fileName.set_filename("fileshare");
     client.InsertFile(fileName);
 
-
     client.GetFiles();
-    std::cout << client.getFileInfoList().fileil(0).filename() << "\t" << client.getFileInfoList().fileil_size()
-              << std::endl;
-    client.ShareFile(client.getFileInfoList().fileil(0).fileidentifier(), user_share.username());
+    client.GetFileContent(client.getFileInfoList().fileil(0));
+    /*
 
-    client.GetFiles();
-    std::cout << client.getFileInfoList().fileil(0).filename() << "\t" << client.getFileInfoList().fileil_size() << "\t" << client.getFileInfoList().fileil(0).usernameo()
-              << std::endl;
-
-    */
     QApplication a(argc, argv);
     Principale main;
 
